be7156019427c48abfb75982737c9e45abb435dd
YERIM <dnaer456@gmail.com>
2020-11-03 21:40:58 +0900

    js

--- graphql/model/User.js
+++ graphql/model/User.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
+// const <--변하지 않는다는 의미  Schema <-- 객체는 대문자로 시작함
 
-const User = new Schema(
+const User = new Schema( // User라는 상수에 스키마를 만듬
   {
     email: {
       type: String,
@@ -29,3 +30,4 @@ const User = new Schema(
 );
 
 export default mongoose.model(`User`, User, `User`);
+// (`별칭`, 실제 객체 , `몽고DB안에 있는 진짜 별칭스키마는 뭔데?`)
