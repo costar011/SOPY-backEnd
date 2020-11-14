86f47881808e7bf4f090d2f966986599e36c0038
YERIM <dnaer456@gmail.com>
2020-11-14 21:04:18 +0900

    app.js description

--- graphql/schemas.js
+++ graphql/schemas.js
@@ -1,4 +1,4 @@
-import path from "path";
+import path from "path"; // path->경로 경로를 추적할 수 있는 것을 impoert함
 import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 
--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
-import express from "express";
-import morgan from "morgan";
-import dotenv from "dotenv";
-dotenv.config();
+import express from "express"; // express framework를 사용하기 위해 import함
+import morgan from "morgan"; // debugging을 위해 morgan을 import함
+import dotenv from "dotenv"; // dotenv를 쓰기 위해 import함
+dotenv.config(); // dotenv.config() 함수 실행
 import { graphqlHTTP } from "express-graphql";
 import cors from "cors";
 import bodyParser from "body-parser";
 import schema from "../graphql/schemas";
 import connect from "../db/mongo";
 
+// express를 app에 넣는다.
 const app = express();
 
 app.set(`PORT`, process.env.PORT);
+
+// app.js 에게 morgan를 써야한다고 신호를 줌
+// dev <- 키워드
 app.use(morgan(`dev`));
 
+// connect(); <--- 함수
 connect();
 
 app.use(
@@ -25,6 +30,7 @@ app.use(
   })
 );
 
+// 설정 끝난 후 Server Start
 app.listen(app.get(`PORT`), () => {
   console.log(
     `[SOPY SERVER START]:: ${process.env.PORT}, WITH GraphQL - MongoDB`
